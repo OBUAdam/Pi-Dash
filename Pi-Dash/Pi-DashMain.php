7591a5f991e05cc8d4423bdb9b00dd5aac1a3242
OBUAdam <AdamBCollins@gmail.com>
2016-01-02 23:07:42 -0600

    Got Recent Things stuff working.

--- Pi-Dash/Pi-DashMain.php
+++ Pi-Dash/Pi-DashMain.php
@@ -3,8 +3,27 @@
 
 <?php
 	$SettingsDir = '/var/www/html/Settings';
-	$CalendarAccountFile = 'CalendarAccount.supersecret';
-	$CalendarAccount = fopen($SettingsDir.$CalendarAccountFile, "r") or die("Unable to open Calendar Account file!");
+	$CalendarAccountFileName = '/CalendarAccount.supersecret';
+	$CalendarAccountFile = fopen($SettingsDir.$CalendarAccountFileName, "r") or die("Unable to open Calendar Account file!");
+	$CalendarAccount = fread($CalendarAccountFile, filesize($SettingsDir.$CalendarAccountFileName)) or die("Unable to read Calendar Account file!");
+	
+	$RecentThingsDir = '/var/www/html/Pi-DashFiles';
+	$RecentThingsFileName = '/RecentThings.txt';
+	$RecentThingsFile = fopen($RecentThingsDir.$RecentThingsFileName, "r") or die("Unable to open Recent Things file!");
+	$RecentThingsString = fread($RecentThingsFile, filesize($RecentThingsDir.$RecentThingsFileName)) or die("Unable to read Recent Things file!");
+	$RecentThings = explode("----", $RecentThingsString);
+	
+	$ThingsList = "<ul id=\"RecentThingsList\">";
+	foreach ($RecentThings as &$Thing)
+	{
+		$ThingParts = explode("|||", $Thing);
+		
+		if(count($ThingParts) == 2)
+		{
+			$ThingsList = $ThingsList . "<li>From: " . $ThingParts[0] . " Subject: " . $ThingParts[1] . "</li>";
+		}
+	}
+	$ThingsList = $ThingsList . "</ul>";
 ?>
 
 <head>
@@ -113,10 +132,39 @@
 	    width    : 940px;
 	    height   : 980px;
 	}
+	
+	#RecentThingsListDiv
+	{
+		background-color: #7e7e7e;
+		width    : 525px;
+		height: auto;
+		float: left;
+	}
+	#RecentThingsList
+	{
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+	}
+	li
+	{
+		font: 200 20px/1.5 Helvetica, Verdana, sans-serif;
+		border-bottom: 1px solid #ccc;
+		background-color: #b5b5b5;
+	}
+	li:last-child
+	{
+		border: none;
+	}
+	#RecentThingsHeader
+	{
+		font: 400 40px/1.5 Helvetica, Verdana, sans-serif;
+		margin: 0;
+		padding: 0;
+	}
 </style>
 </head>
 <body style="background-color: darkgrey">
-<?php echo $CalendarAccount ?>
 <div id="DateAndTimeDiv">
 	<iframe id="DateAndTimeIFrame" src="http://www.timeanddate.com/worldclock/fullscreen.html?n=184"></iframe>
 </div>
@@ -130,11 +178,15 @@
 	<iframe id="WazeIFrame"	src="https:/embed.waze.com/iframe?zoom=12&lat=35.41941&lon=-97.52289"></iframe>
 </div>
 <div id="CalendarDiv">
-	<iframe id="CalendarIFrame" src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=EMAILACCOUNT%40EMAILPROVIDER.com&amp;color=%235F6B02&amp;ctz=America%2FChicago" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+	<iframe id="CalendarIFrame" src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=<?php echo $CalendarAccount; ?>&amp;color=%235F6B02&amp;ctz=America%2FChicago" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 </div>
 <div>
 	<a class="twitter-timeline" href="https://twitter.com/WazeTrafficOKC" data-widget-id="683439288919298048">Tweets by @WazeTrafficOKC</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</div>
+<div id="RecentThingsListDiv">
+	<h2 id="RecentThingsHeader">Recent Things:</h2>
+	<?php echo $ThingsList; ?>
 </div>
 </body>
 </html>
--- Pi-Dash/Pi-DashRecentThings.php
+++ Pi-Dash/Pi-DashRecentThings.php
@@ -1,18 +1,14 @@
 <?php
-echo 'Hello ' . htmlspecialchars($_GET["name"]) . '!';
 try
 {
-	echo 'START!';
 	error_reporting(E_ALL);
 	ini_set("display_errors", "on");
 	
 	$dir = '/var/www/html/Pi-DashFiles';
-	$FileName = '/Test.txt';
+	$FileName = '/RecentThings.txt';
  	
- 	file_put_contents ($dir.$FileName, htmlspecialchars($_GET["name"]), FILE_APPEND);
-	echo $FileName;
+ 	file_put_contents ($dir.$FileName, htmlspecialchars($_GET["From"]) . "|||" . htmlspecialchars($_GET["Subject"]) . "----", FILE_APPEND);
 } catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
-echo 'Hello ' . htmlspecialchars($_GET["name"]) . 'END!';
 ?>
